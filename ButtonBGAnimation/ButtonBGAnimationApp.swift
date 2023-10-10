ba7c12eed7406f78be8bc95f9d299ac5aa5303fd
lord-of-the-lost <ignatov.99@yandex.ru>
2023-10-10 18:19:28 +0300

    Initial Commit

---
+++ ButtonBGAnimation/ButtonBGAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonBGAnimationApp.swift
+//  ButtonBGAnimation
+//
+//  Created by Николай Игнатов on 10.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ButtonBGAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
